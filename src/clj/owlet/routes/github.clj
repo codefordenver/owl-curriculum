b6f7842044579f24bb7bc2b07b4b7abf07719801
David Viramontes <dviramontes@gmail.com>
2017-09-07 21:28:48 -0600

    feat: proxy github stats request from back-end

---
+++ src/clj/owlet/routes/github.clj
@@ -0,0 +1,21 @@
+(ns owlet.routes.github
+  (:require [compojure.core :refer [defroutes GET]]
+            [ring.util.http-response :refer [ok]]
+            [compojure.api.sweet :refer [context]]
+            [org.httpkit.client :as http]
+            [cheshire.core :as json]))
+
+(def OWLET_GITHUB_TOKEN (System/getenv "OWLET_GITHUB_TOKEN"))
+
+(defn proxy-github-request
+  "proxy front end request"
+  [_]
+  (let [headers {:Authorization OWLET_GITHUB_TOKEN}
+        {:keys [status body]} @(http/get "https://api.github.com/repos/codefordenver/owlet/stats/code_frequency" headers)]
+    (when (= status 200)
+      (ok {:status status
+           :body   (json/parse-string body true)}))))
+
+
+(defroutes routes
+           (GET "/stats" {params :params} proxy-github-request))
