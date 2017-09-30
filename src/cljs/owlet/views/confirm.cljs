018e5b2a5a5cb7f2b6d24f43ba881fb87f165a4a
ZadenRB <zaden.ruggieroboune@gmail.com>
2017-09-30 16:45:55 -0600

    Fixes recaptcha to use a proper cljs function

--- src/cljs/owlet/views/confirm.cljs
+++ src/cljs/owlet/views/confirm.cljs
@@ -1,13 +1,28 @@
 (ns owlet.views.confirm
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [reagent.core :as r]
+            [clojure.spec :as s]))
 
-(defn ^:export data_callback []
+(defn data-callback []
     (let [id @(rf/subscribe [:subscriber-info])]
       (set! (.-location js/document) (str "http://owlet.codefordenver.org/api/contentful/webhook/content/confirm?id=" id))))
 
+(s/fdef re-captcha
+        :args (s/cat :dom-id string?
+                     :callback ifn?))
+
+(defn re-captcha [dom-id callback]
+  (if (.getElementById js/document dom-id)
+    (.render js/grecaptcha dom-id
+             #js {:sitekey "6LdKwjAUAAAAAOXp-DRhuRXN77yKgZ9vDTR5gcxl"
+                  :callback callback})))
+
 (defn confirm-view []
-  [:div#confirm
-   [:span#confirm-message "Please check the box below to confirm your subscription"]
-   [:div.g-recaptcha
-     {:data-sitekey "6LdKwjAUAAAAAOXp-DRhuRXN77yKgZ9vDTR5gcxl"
-      :data-callback "recaptchaCallback"}]])
+  (r/create-class
+       {:component-did-mount
+        #(re-captcha "g-recaptcha" data-callback)
+        :reagent-render
+         (fn []
+          [:div#confirm
+           [:span#confirm-message "Please check the box below to confirm your subscription"]
+           [:div#g-recaptcha]])}))
