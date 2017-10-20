569981f096559e6e244b324245e2eb55e0a26b84
eemshi <oreneemshi@gmail.com>
2017-10-19 20:47:54 -0600

    feat: reusable klipse component

--- src/cljs/owlet/views/interactive.cljs
+++ src/cljs/owlet/views/interactive.cljs
@@ -1,14 +1,5 @@
 (ns owlet.views.interactive
-  (:require [reagent.core :as reagent]))
+  (:require [owlet.components.interactive.klipse :refer [klipse-component]]))
 
 (defn interactive-view []
-  (reagent/create-class
-     {:component-did-mount
-      (fn []
-        (let [tag (js/document.createElement "script")]
-          (-> tag (.setAttribute "src" "https://storage.googleapis.com/app.klipse.tech/plugin_prod/js/klipse_plugin.min.js"))
-          (js/document.body.appendChild tag)))
-      :reagent-render
-      (fn []
-        [:pre
-         [:code {:class "language-klipse-python"} "x = 1 + 1\nprint(x)"]])}))
+  [klipse-component "language-klipse-python" "x = 1 + 1\nprint(x)"])
