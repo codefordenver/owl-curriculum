85e125ff51bba0872ceb7749f63919bc641f285d
eemshi <oreneemshi@gmail.com>
2017-10-24 11:58:21 -0600

    adds js evaluator to klipse

--- src/cljs/owlet/views/interactive.cljs
+++ src/cljs/owlet/views/interactive.cljs
@@ -2,4 +2,6 @@
   (:require [owlet.components.interactive.klipse :refer [klipse-component]]))
 
 (defn interactive-view []
-  [klipse-component "language-klipse-python" "x = 1 + 1\nprint(x)"])
+  [:div
+   [klipse-component "language-klipse-python" "x = 1 + 1\nprint(x)"]
+   [klipse-component "language-klipse-eval-js" "var x = 1 + 1\nconsole.log(x)"]])
