6adaf44673b72329fb2315337e46e0fb07d655cf
David Viramontes <dviramontes@gmail.com>
2018-01-01 19:21:48 -0700

    feat: change eval code output box with :ok and :err event dispatch changes, chore: adds generated klipse plugin code

--- src/cljs/owlet/components/interactive/klipse.cljs
+++ src/cljs/owlet/components/interactive/klipse.cljs
@@ -1,13 +1,23 @@
 (ns owlet.components.interactive.klipse
-  (:require-macros
-    [purnam.core :refer [?]])
+  (:require-macros [purnam.core :refer [? !> !]])
   (:require [reagent.core :as reagent]
             [clojure.string :as string]
             [owlet.components.loading :refer [loading-component]]))
 
-(def klipse-plugin "js/vendor/klipse_plugin.min.js")
+(def klipse-plugin-path "js/vendor/klipse_plugin.min.js")
 
 (def klipse-container-class ".klipse-container")
+
+(def ok-color
+  ;; $aqua
+  "#65D2C0")
+
+(def err-color
+  ;; $magenta
+  "#FF0076")
+
+(defn- reset-background-color! [element color]
+  (! element.style.background color))
 
 (defn klipse-component [language code]
   (let [evaled? (reagent/atom false)]
@@ -15,7 +25,7 @@
       {:component-did-mount
        (fn []
          (let [tag (js/document.createElement "script")]
-           (-> tag (.setAttribute "src" klipse-plugin))
+           (-> tag (.setAttribute "src" klipse-plugin-path))
            (js/document.body.appendChild tag)
            (js/setTimeout #(reset! evaled? true) 5000)))
        :reagent-render
@@ -26,11 +36,13 @@
               (. klipse-container
                  (addEventListener "klipse-snippet-evaled"
                                    (fn [e]
-                                     (let [text (? e.detail.state.result-element.display.maxLine.text)
-                                           has-error? (not (nil? (re-find #"error" text)))]
-                                       (js/console.log has-error?)
-                                       (js/console.log text)))))))
-
+                                     (let [output-wrapper-div (? e.detail.result-element.display.wrapper)
+                                           detail (? e.detail.result)
+                                           [result _] (js->clj detail)
+                                           result->key (keyword result)]
+                                       (if (= result->key :ok)
+                                         (reset-background-color! output-wrapper-div ok-color)
+                                         (reset-background-color! output-wrapper-div err-color))))))))
           [:pre
            [:code {:class (case language
                             "Python" "language-klipse-python"
