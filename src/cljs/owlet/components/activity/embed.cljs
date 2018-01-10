1d8d6792919d08983f22a88ca37051e128138944
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-01-10 09:18:57 -0700

    Adds postMessage to slides when rendering a klipseActivity, WIP handler of messages from slideshow

--- src/cljs/owlet/components/activity/embed.cljs
+++ src/cljs/owlet/components/activity/embed.cljs
@@ -1,20 +1,37 @@
 (ns owlet.components.activity.embed
   (:require [re-frame.core :as rf]))
 
-(defn- generic-responsive-iframe
-  "returns an responsive iframe"
-  [iframe-code]
-  (.replace iframe-code (js/RegExp. "/\\\"/g,'\\''")))
+(defn check-slides [iframe-code]
+  (let [cleaned-iframe (clojure.string/replace iframe-code #"\"" "'")
+        iframe-url (re-find #"//.*?[^']*" cleaned-iframe)]
+    (if-not (nil? (re-find #"slides.com" iframe-url))
+        (if (nil? (re-find #"\?" iframe-url))
+          (let [post-message-url (str iframe-url "?postMessageEvents=true")]
+            (clojure.string/replace cleaned-iframe iframe-url post-message-url))
+          (let [post-message-url (str iframe-url "&postMessageEvents=true")]
+            (clojure.string/replace cleaned-iframe iframe-url post-message-url)))
+        cleaned-iframe)))
+
+(defn post-message-handler [e]
+  (let [data (.-data e)
+        slideshow (.querySelector js/document "iframe")]
+    (prn data)))
 
 (defn activity-embed [embed tags preview]
-  (let [preview-url (-> preview :sys :url)]
+  (let [preview-url (-> preview :sys :url)
+        activity @(rf/subscribe [:activity-in-view])
+        activity-type (get-in activity [:sys :contentType :sys :id])]
     [:div.activity-embed-wrap.box-shadow
      (if-not embed
        [:div.activity-preview
         [:img {:src preview-url :width "100%"}]]
        [:div.embed-container
         {"dangerouslySetInnerHTML"
-         #js{:__html (generic-responsive-iframe embed)}}])
+         #js{:__html (case activity-type
+                       "klipseActivity" (check-slides embed)
+                       "activity" embed)}}
+        (when (= activity-type "klipseActivity")
+          (.addEventListener js/window "message" post-message-handler))])
      (when tags
        [:div.activity-tags-wrap
         [:div.tags
