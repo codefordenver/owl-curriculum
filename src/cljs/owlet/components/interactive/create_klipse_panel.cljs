7416dd41ba0771c6d8350eea7abccefbe5e175da
eemshi <oreneemshi@gmail.com>
2018-01-14 01:52:40 -0700

    styles create-klipse-panel-activity view

--- src/cljs/owlet/components/interactive/create_klipse_panel.cljs
+++ src/cljs/owlet/components/interactive/create_klipse_panel.cljs
@@ -5,21 +5,15 @@
 (defn create-klipse-panel-component [order]
   [:div.activity-info-wrap.box-shadow
      [:h2 (str order ". ")
-      [:input {:type "text"
-               :name "panel-heading"
-               :placeholder "Heading"}]]
+      [:textarea {:rows "1"
+                  :placeholder "Heading"}]]
      [:div.panel-comments
         ;TODO: Replace with markdown editor (simplemde)
         [:h5 "optional markdown"]]
      [:div.code-block
         [:textarea {:rows "2"
-                    :style {:width "100%"}
                     :placeholder "optional code block"}]]
      [klipse-component "Python" "# replace with starting code"]
      [:div.panel-comments
         ;TODO: Replace with markdown editor (simplemde)
-        [:h5 [:b "Fun fact: "]
-             "Printing \"Hello world!\" to the screen as your first program in a new language is considered good luck ~(˘▾˘)~"]
-        [:h5 [:b "Values in \"quotes\" are called "
-              [:i "strings. "]]
-             "\"Hello world!\" is a string."]]])
+        [:h5 "optional markdown"]]])
--- src/cljs/owlet/views/create_klipse_panel_activity.cljs
+++ src/cljs/owlet/views/create_klipse_panel_activity.cljs
@@ -8,16 +8,16 @@
    [:div.activity-wrap
     [:div.activity-header.col-xs-12
      [:div.activity-title-wrap
-      [:h1 [:mark.white.box-shadow [back]]
-           [:input {:type "text"
-                    :name "title"
-                    :placeholder "Activity Title"}]]
+      [:h1 [:mark.white.box-shadow [back]
+            [:input {:type "text"
+                     :name "title"
+                     :placeholder "Activity Title"}]]]
       [:h5.author "Created by: "
        [:input {:type "text"
                 :name "author"
                 :placeholder "Author"}]]]]
-    [:div.activity-content.col-xs-12.col-lg-7
+    [:div.activity-content.col-sm-12.col-lg-7
      [create-klipse-panel-component 1]
      [create-klipse-panel-component 2]
      ;TODO: When clicked, call [create-klipse-panel-component] with the next number in sequence
-     [:button {:style {:margin-bottom "3em"}} "+ Add interactive panel"]]]])
+     [:button.add-panel {:style {:margin-bottom "3em"}} "+ Add another panel"]]]])
