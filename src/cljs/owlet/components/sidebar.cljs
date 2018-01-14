769ab727af6fe2ca71a487f86a4080d0f8a5b632
David Viramontes <dviramontes@gmail.com>
2018-01-14 00:26:17 -0700

    refactor: removes unused deps and decomplects component

--- src/cljs/owlet/components/sidebar.cljs
+++ src/cljs/owlet/components/sidebar.cljs
@@ -1,21 +1,17 @@
 (ns owlet.components.sidebar
-  (:require [owlet.components.login :refer [login-component]]
-            [re-frame.core :as rf]
-            [re-com.core :as re-com :refer-macros [handler-fn]]
-            [re-com.popover]
-            [reagent.core :as reagent]))
+  (:require [owlet.components.login :refer [login-component]]))
 
 (defn sidebar-component []
-  (let [showing-branch? (reagent/atom false)
-        showing-about? (reagent/atom false)]
-    [:div#sidebar
-     [:div#owlet-logo-div
-      [:a#owlet-image {:href "#"}
-        [:img#owlet-owl {:src "../img/owlet-owl.png"}]]]
-     [:div.menu
-      [:div.login
-       [login-component]]
-      [:a.navigation.branch-icon {:href "#/branches"}
-       [:div.branch-icon]]
-      [:a.navigation.about-icon {:href "#/about"}
-       [:div.about-icon]]]]))
+  [:div#sidebar
+   [:div#owlet-logo-div
+    [:a#owlet-image {:href "#"}
+      [:img#owlet-owl {:src "../img/owlet-owl.png"}]]]
+   [:div.menu
+    [:div.login
+     [login-component]]
+    [:a.navigation.branch-icon {:href "#/branches"}
+     [:div.branch-icon]]
+    [:a.navigation.about-icon {:href "#/settings"}
+     [:div.about-icon]]
+    [:a.navigation.about-icon {:href "#/about"}
+     [:div.about-icon]]]])
