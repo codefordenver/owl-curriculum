5684a41322dffcef9437b79c2682ef5027ec28a7
dviramontes <davidv@keepkey.com>
2018-02-13 22:20:21 -0700

    feat: basic role assigment, closes #353

--- src/cljs/owlet/views/settings.cljs
+++ src/cljs/owlet/views/settings.cljs
@@ -2,54 +2,58 @@
   (:require [reagent.core :refer [atom]]
             [re-com.core :refer [checkbox]]
             [re-frame.core :as rf]
-            [owlet.firebase :as fb]
             [owlet.views.login-only :refer [login-only-view]]))
 
-(defn settings [roles]
-  [:div.information-wrap
-   [:div.information-inner-wrap.col-xs-12.col-sm-11
-    [:div.information-inner
-     [:div.inner-height-wrap
-      [:h1 [:mark "My Settings"]]
-      [:h2 "Choose role(s):"]
-      [checkbox
-       :style {:align-self "center"}
-       :label "Student"
-       :model     (:student @roles)
-       :on-change #(swap! roles update :student not)]
+(defn settings []
+  (let [my-roles (rf/subscribe [:my-roles])
+        role-assignment #(contains? (set @my-roles) (name %))
+        role-change-handler
+        (fn [new-role]
+          (let [new-role (name new-role)
+                role-list (set @my-roles)]
+            (if (contains? role-list new-role)
+              (rf/dispatch [:update-user-roles! (disj role-list new-role)])
+              (rf/dispatch [:update-user-roles! (conj role-list new-role)]))))]
+    (fn []
+      [:div.information-wrap
+       [:div.information-inner-wrap.col-xs-12.col-sm-11
+        [:div.information-inner
+         [:div.inner-height-wrap
+          [:h1 [:mark "My Settings"]]
+          [:h2 "Choose role(s):"]
 
-      [checkbox
-       :style {:align-self "center"}
-       :label "Teacher"
-       :model     (:teacher @roles)
-       :on-change #(swap! roles update :teacher not)]
-      [checkbox
-       :style {:align-self "center"}
-       :label "Content Creator"
-       :model     (:content-creator @roles)
-       :on-change #(swap! roles update :content-creator not)]
-      (when (:content-creator @roles)
-       [:div.settings-section
-        [:h3 "Create Activity"]
-        [:ul
-         [:li "Interactive:"]
-         [:ul
-          [:li [:a {:href "#/create/klipse-panel-activity"}
-                 "Coding, Multi-Panel"]
-               " — Python, JavaScript, and/or Clojure ["
-               [:a {:href "#/activity/hello-world"} "Example"]
-               "]"]]]])]]]])
+          [checkbox
+           :style {:align-self "center"}
+           :label "Student"
+           :model (role-assignment :student)
+           :on-change #(role-change-handler :student)]
+
+          [checkbox
+           :style {:align-self "center"}
+           :label "Teacher"
+           :model    (role-assignment :teacher)
+           :on-change #(role-change-handler :teacher)]
+
+          [checkbox
+           :style {:align-self "center"}
+           :label "Content Creator"
+           :model     (role-assignment :content-creator)
+           :on-change #(role-change-handler :content-creator)]
+
+          (when (contains? (set @my-roles) :content-creator)
+            [:div.settings-section
+             [:h3 "Create Activity"]
+             [:ul
+              [:li "Interactive:"]
+              [:ul
+               [:li [:a {:href "#/create/klipse-panel-activity"}
+                     "Coding, Multi-Panel"]
+                " — Python, JavaScript, and/or Clojure ["
+                [:a {:href "#/activity/hello-world"} "Example"]
+                "]"]]]])]]]])))
 
 
 (defn settings-view []
-  (let [roles (atom {:content-creator false
-                     :teacher false
-                     :student false})
-        _ (add-watch roles :watcher
-                     (fn [key atm _ new-state]
-                       (when (apply fb/legal-db-value? (map val new))
-                          (prn "valid"))))]
-    (fn []
-      (if @(rf/subscribe [:my-id])
-        [settings roles]
-        [login-only-view]))))
+  (if @(rf/subscribe [:my-id])
+    [settings]
+    [login-only-view]))
