ff9fae7c664dc70a270a5c4dbf8229441375445a
eemshi <oreneemshi@gmail.com>
2018-03-22 18:05:05 -0600

    refactor: simplemde component

--- src/cljs/owlet/components/creation/create_klipse_panel.cljs
+++ src/cljs/owlet/components/creation/create_klipse_panel.cljs
@@ -3,45 +3,37 @@
             [owlet.components.creation.custom-klipse-component :refer [custom-klipse-component]]
             [owlet.components.creation.create-klipse-code-validation :refer [create-klipse-code-validation-component]]
             [re-com.core :refer [h-box button]]
-            cljsjs.simplemde))
+            [owlet.components.creation.markdown-editor :refer [simplemde]]))
 
-(defn create-klipse-panel-component [panel-number]
-  (let [text-id-base (str "panel-" panel-number "-text-")]
-    (reagent/create-class
-      {:component-did-mount
-       (fn []
-         (let [smde-1-id (str text-id-base "1")
-               smde-2-id (str text-id-base "2")
-               smde-1 (js/SimpleMDE. #js {:element (js/document.querySelector (str "#" smde-1-id))
-                                          :lineWrapping true
-                                          :autosave #js {:enabled true
-                                                         :uniqueId smde-1-id}})
-               smde-2 (js/SimpleMDE. #js {:element (js/document.querySelector (str "#" smde-2-id))
-                                          :lineWrapping true
-                                          :autosave #js {:enabled true
-                                                         :uniqueId smde-2-id}})]))
-       :reagent-render
-       (fn [panel-number]
-        [:div.activity-creation-wrap
-         [:div.panel-heading.flexcontainer-wrap
-          [h-box
-           :size "1"
-           :align :start
-           :children [[:div.panel-number (str panel-number)]
-                      [:div {:style {:width "100%"}}
-                       [:h2 [:textarea {:id (str "panel-" panel-number "-heading")
-                                        :rows "2"
-                                        :placeholder "Heading"}]]]]]]
-         [:div.panel-text
-          [:h5 [:mark "Optional Text"]]
-          [:textarea {:id (str text-id-base "1")
-                      :placeholder "Accepts markdown"}]]
-         [:div.panel-klipse
-          [custom-klipse-component panel-number]]
-         [:div.panel-validation
-          [:h6 "Below, enter the expected output for this panel:"]
-          [create-klipse-code-validation-component false]]
-         [:div.panel-text
-          [:h5 [:mark "Optional Text"]]
-          [:textarea {:id (str text-id-base "2")
-                      :placeholder "Accepts markdown"}]]])})))
+(defn create-klipse-panel-component [id]
+  (reagent/create-class
+    {:component-did-mount
+     (fn []
+       (let [smde-text1 (simplemde "text1")
+             _ (set! js/window.foo smde-text1)
+             smde-text2 (simplemde "text2")]))
+     :reagent-render
+     (fn [id]
+      [:div.activity-creation-wrap
+       [:div.panel-heading.flexcontainer-wrap
+        [h-box
+         :size "1"
+         :align :start
+         :children [[:div.panel-number (str id)]
+                    [:div {:style {:width "100%"}}
+                     [:h2 [:textarea {:id (str "panel-" id "-heading")
+                                      :rows "2"
+                                      :placeholder "Heading"}]]]]]]
+       [:div.panel-text
+        [:h5 [:mark "Optional Text"]]
+        [:textarea {:id "text1"
+                    :placeholder "Accepts markdown"}]]
+       [:div.panel-klipse
+        [custom-klipse-component id]]
+       [:div.panel-validation
+        [:h6 "Below, enter the expected output for this panel:"]
+        [create-klipse-code-validation-component false]]
+       [:div.panel-text
+        [:h5 [:mark "Optional Text"]]
+        [:textarea {:id "text2"
+                    :placeholder "Accepts markdown"}]]])}))
