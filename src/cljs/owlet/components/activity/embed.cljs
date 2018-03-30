8f16b156a0fd44ce681a1930ca4cd58e5408f92a
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-03-29 23:05:40 -0600

    Fixes Next Slide Button (#425)
    * Adds more logging for debugging
    
    * Changes prn to console.log for errors
    
    * Prints indexh atom
    
    * Attempts to use normal atoms to resolve issue
    
    * Uses function to access event property to avoid broken slide change handler

--- src/cljs/owlet/components/activity/embed.cljs
+++ src/cljs/owlet/components/activity/embed.cljs
@@ -50,10 +50,10 @@
       (reset! valid? true))))
 
 (defn handle-slide-change [e]
-  (when (> (.-newIndexh e) @indexh)
+  (when (> (? e.newIndexh) @indexh)
     (if @valid?
       (let [activity @(rf/subscribe [:activity-in-view])]
-        (reset! indexh (.-newIndexh e))
+        (reset! indexh (? e.newIndexh))
         (let [prev-expected-output (get-in activity [:fields :codeValidation (keyword (str (- @indexh 1)) :expectedOutput)])]
           (if-let [new-expected-output (get-in activity [:fields :codeValidation (keyword (str @indexh)) :expectedOutput])]
             (reset! valid? (= prev-expected-output new-expected-output))
