783992a829a385b311004458677ed8b0c960586e
eemshi <oreneemshi@gmail.com>
2018-03-29 21:21:12 -0600

    renders first 4 activities in Featured section, fixes activity thumbnail styles

--- src/cljs/owlet/views/welcome.cljs
+++ src/cljs/owlet/views/welcome.cljs
@@ -1,23 +1,34 @@
 (ns owlet.views.welcome
   (:require [owlet.components.activity-thumbnail :refer [activity-thumbnail]]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [reagent.core :as reagent]))
 
 (defn welcome-view []
-  (let [activities @(rf/subscribe [:activities])]
-    [:div.welcome-container
-     [:div.welcome-wrap
-      [:div.welcome-image ""]
-      [:div.welcome-text
-       [:h1 [:mark "Welcome to Owlet!"]]
-       [:p "Explore free, self-guided projects for creative learning in "
-        [:b "STEM:"]
-        " Science, Technology, Engineering, Art, and Math!"]
-       [:p "Explora proyectos creativos para aprender ciencia, tecnología, arte y mate. ¡Gratis!"]]]
-     [:div.featured-projects-wrap
-      [:h2 "Featured Projects"]
-      [:div.grid
-       (for [activity activities
-             :let [fields (:fields activity)
-                   entry-id (get-in activity [:sys :id])]]
-         ^{:key [entry-id (gensym "key-")]}
-         [activity-thumbnail fields entry-id "featured"])]]]))
+  (reagent/create-class
+     {:component-did-mount
+      (fn []
+        (js/Masonry. (js/document.querySelector ".grid")
+                     #js {:itemSelector ".grid-item"}))
+      :component-did-update
+      (fn []
+        (js/Masonry. (js/document.querySelector ".grid")
+                     #js {:itemSelector ".grid-item"}))
+      :reagent-render
+      (fn []
+        (let [activities (take 3 @(rf/subscribe [:activities]))]
+          [:div.welcome-container
+           [:div.welcome-wrap
+            [:div.welcome-image ""]
+            [:div.welcome-text
+             [:h1 [:mark "Welcome to Owlet!"]]
+             [:p "Explore free, self-guided projects for creative learning in "
+              [:b "STEM: Science, Technology, Engineering, Art, and Math!"]]
+             [:p "Explora proyectos creativos para aprender ciencia, tecnología, arte y mate. ¡Gratis!"]]]
+           [:div.featured-projects-wrap
+            [:h2 "Featured Projects"]
+            [:div.grid
+             (for [activity activities
+                   :let [fields (:fields activity)
+                         entry-id (get-in activity [:sys :id])]]
+               ^{:key [entry-id (gensym "key-")]}
+               [activity-thumbnail fields entry-id "featured"])]]]))}))
