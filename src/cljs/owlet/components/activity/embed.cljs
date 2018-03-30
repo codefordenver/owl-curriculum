f9cd6972347d2916df7c8d8808f0369c052fe659
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-03-29 21:54:39 -0600

    Changes prn to console.log for errors

--- src/cljs/owlet/components/activity/embed.cljs
+++ src/cljs/owlet/components/activity/embed.cljs
@@ -50,18 +50,18 @@
       (reset! valid? true))))
 
 (defn handle-slide-change [e]
-  (prn (> (.-newIndexh e) @indexh))
+  (js/console.log (> (.-newIndexh e) @indexh))
   (when (> (.-newIndexh e) @indexh)
     (if @valid?
       (let [activity @(rf/subscribe [:activity-in-view])]
-        (prn "Valid")
+        (js/console.log "Valid")
         (reset! indexh (.-newIndexh e))
         (let [prev-expected-output (get-in activity [:fields :codeValidation (keyword (str (- @indexh 1)) :expectedOutput)])]
           (if-let [new-expected-output (get-in activity [:fields :codeValidation (keyword (str @indexh)) :expectedOutput])]
             (reset! valid? (= prev-expected-output new-expected-output))
             (reset! valid? true))))
       (do
-        (prn "Not Valid")
+        (js/console.log "Not Valid")
         (.preventDefault e)))))
 
 (defn activity-embed [embed tags preview]
