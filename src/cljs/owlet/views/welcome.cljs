1a2db105a522c085cf9089683a00deb6d6a9ff88
Michelle L <oreneemshi@gmail.com>
2018-04-04 17:30:12 -0600

    Feat: New welcome view design (#426)
    * begins new welcome view with featured projects
    
    * renders first 4 activities in Featured section, fixes activity thumbnail styles
    
    * adds owl image & responsiveness to welcome view
    
    * Add random activites to welcome page & sign up button
    
    * Adds necessary function to find JSON property (#424)
    
    * Adds necessary function to find JSON property
    
    * Adds logging to debug
    
    * rm: prn
    
    * Fixes Next Slide Button (#425)
    
    * Adds more logging for debugging
    
    * Changes prn to console.log for errors
    
    * Prints indexh atom
    
    * Attempts to use normal atoms to resolve issue
    
    * Uses function to access event property to avoid broken slide change handler
    
    * add sign-up button, refactor auth0/lock to pass in initialScreen

--- src/cljs/owlet/views/welcome.cljs
+++ src/cljs/owlet/views/welcome.cljs
@@ -1,14 +1,39 @@
 (ns owlet.views.welcome
-  (:require
-    [owlet.components.login :refer [login-component]]
-    [re-frame.core :as rf]))
+  (:require [owlet.components.activity-thumbnail :refer [activity-thumbnail]]
+            [owlet.components.login :refer [signup-button]]
+            [re-frame.core :as rf]
+            [reagent.core :as reagent]))
 
 (defn welcome-view []
-  [:div.welcome-wrap
-    [:div.user-type
-      [:p#largetext.text-shadow "Welcome to Owlet"]
-      [:div.welcome-text.text-shadow
-        [:p "Explore some of the awesome things you can do with multimedia & coding!"]
-        [:p "¡Bienvenidx a Owlet! Explora lo que puedes hacer con programación y con multimedios."]]
-      [:a {:href "#/branches"}
-        [:button.btn.btn-branches "Go to Activities"]]]])
+  (reagent/create-class
+     {:component-did-mount
+      (fn []
+        (js/Masonry. (js/document.querySelector ".grid")
+                     #js {:itemSelector ".grid-item"}))
+      :component-did-update
+      (fn []
+        (js/Masonry. (js/document.querySelector ".grid")
+                     #js {:itemSelector ".grid-item"}))
+      :reagent-render
+      (fn []
+        (let [activities (take 3 (shuffle @(rf/subscribe [:activities])))]
+          [:div.welcome-container
+           [:div.welcome-wrap
+            [:div.welcome-image
+             [:img {:src "img/owlet-owl.png"}]]
+            [:div.welcome-text
+             [:h1 [:mark "Welcome to Owlet!"]]
+             [:p "Explore free, self-guided projects for creative learning in "
+              [:b "STEM: Science, Technology, Engineering, Art, and Math!"]]
+             [:p "Explora proyectos creativos para aprender ciencia, tecnología, arte y mate. ¡Gratis!"]
+             [signup-button]
+             [:button.btn-explore {:href "/#/branches"}
+              "Explore"]]]
+           [:div.featured-projects-wrap
+            [:h2 "Featured Projects"]
+            [:div.grid
+             (for [activity activities
+                   :let [fields (:fields activity)
+                         entry-id (get-in activity [:sys :id])]]
+               ^{:key [entry-id (gensym "key-")]}
+               [activity-thumbnail fields entry-id "featured"])]]]))}))
