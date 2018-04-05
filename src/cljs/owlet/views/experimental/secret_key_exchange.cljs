ad919ce5b6bc80eaba549e75daf931cbc950ffe4
eemshi <oreneemshi@gmail.com>
2018-04-04 20:15:30 -0600

    begin copy

--- src/cljs/owlet/views/experimental/secret_key_exchange.cljs
+++ src/cljs/owlet/views/experimental/secret_key_exchange.cljs
@@ -25,4 +25,27 @@
 
 (defn activity []
   [:div.secret-key-exchange-experimental-view
+   [:div
+    [:p "When you submit a password or credit card number online, that information gets sent from your computer to another computer (server) over the internet. However, any information traveling over the internet can be intercepted by someone else, in what is called a "
+     [:a {:href "https://us.norton.com/internetsecurity-wifi-what-is-a-man-in-the-middle-attack.html"
+          :target "_blank"}
+      "man-in-the-middle (MITM) attack"]
+     "."]
+    [:p "So when dealing with sensitive information, computers are instructed to scramble (encrypt) the data before sending it out. Once it's received by an authorized server, that server unscrambles (decrypts) the data and uses it however its supposed to."]
+    [:p "To scramble the data, your computer combines it with a random string of bits, called a \"key.\" To unscramble it, the other computer needs to know that key."]
+    [:p "But how do you share that key without exposing it??"]
+    [:p "Instead of sending the key, your computer sends over two numbers: a "
+     [:span.prime "prime number"]
+     ", and a "
+     [:span.base "base number"]
+     ". It doesn't matter if these are intercepted, because they're useless on their own."]
+    [:p "Each computer takes the publicly shared "
+     [:span.base "base number"]
+     ", applies its own secret exponent (which is never shared), and then mods the result by that "
+     [:span.prime "prime number"]
+     ", producing a brand new number. Then, those numbers are publicly exchanged."]
+    [:p "This is where the magic happens. Computer A takes Computer B's number, applies its own secret exponent from before, and then mods it by that "
+     [:span.prime "prime number"]
+     ". Computer B does the same, using Computer A's number and its own secret exponent."]
+    [:p "The final number will be the same for both; this is their encryption key!"]]
    [prime-base-wrapper]])
