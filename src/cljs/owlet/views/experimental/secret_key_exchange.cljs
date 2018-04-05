935d152a336e3cfcdb3dfbb97ff40e50e0edb548
eemshi <oreneemshi@gmail.com>
2018-04-05 11:26:33 -0600

    lay out Alice and Bob inputs and values

--- src/cljs/owlet/views/experimental/secret_key_exchange.cljs
+++ src/cljs/owlet/views/experimental/secret_key_exchange.cljs
@@ -5,7 +5,9 @@
 
 (def base (reagent/atom 0))
 
-(def secret1 (reagent/atom 0))
+(def alice-secret (reagent/atom 0))
+
+(def bob-secret (reagent/atom 0))
 
 (defn input-number [title value]
   [:div.input-group.mb-3
@@ -14,6 +16,7 @@
      {:type "number"
       :placeholder 0
       :min 0
+      :max 10000
       :value @value
       :on-change #(reset! value (js/parseInt (-> % .-target .-value)))}]
     [:span.input-group-text title]]])
@@ -21,7 +24,7 @@
 (defn known-number [title value]
   [:div.input-group.mb-3
    [:div.input-group-prepend
-    [:div.form-control
+    [:div.form-control.known
      value]
     [:span.input-group-text title]]])
 
@@ -43,24 +46,68 @@
      [:span.public "BASE number"]
      ". It doesn't matter if these are intercepted, because they're useless on their own."]]
    [:div#diffie-hellman-grid
-    [:div.prime-wrapper
+    [:div.divider-1]
+    [:div.divider-2]
+    [:div.flex.pa.prime-input
      [input-number "PRIME #" prime]]
-    [:div.base-wrapper
+    [:div.flex.pa.base-input
      [input-number "BASE #" base]]
-    [:h1.alice-name "Alice"]
-    [:div.alice-secret-wrapper-1
-     [input-number "SECRET #" secret1]]
-    [:div.alice-base-wrapper
+    [:div.flex.pa.alice-name
+     [:h1 "ALICE"]]
+    [:div.flex.pa.alice-secret-1
+     [input-number "SECRET #" alice-secret]]
+    [:div.flex.pa.alice-base
      [known-number "BASE #" @base]]
-    [:div.alice-secret-wrapper-2.flex-container
-     [known-number "" @secret1]
-     [:div "%"]]
-    [:div.alice-prime-1
+    [:div.flex.alice-secret-2
+     [:div.exp
+      [known-number "" @alice-secret]]
+     [:div.mod "%"]]
+    [:div.flex.pa.alice-prime-1
      [known-number "PRIME #" @prime]]
-    [:div.alice-equal-1
-     "="]
-    [:div.alice-number
-     [known-number "ALICE'S #" (mod (js/Math.pow @base @secret1) @prime)]]]
+    [:div.flex.alice-equal-1.equal-sign
+     [:div "="]]
+    [:div.flex.pa.alice-number
+     [known-number "ALICE'S #" (mod (js/Math.pow @base @alice-secret) @prime)]]
+    [:div.flex.pa.alice-bob-number
+     [known-number "BOB'S #" (mod (js/Math.pow @base @bob-secret) @prime)]]
+    [:div.flex.alice-secret-3
+     [:div.exp
+      [known-number "" @alice-secret]]
+     [:div.mod "%"]]
+    [:div.flex.pa.alice-prime-2
+     [known-number "PRIME #" @prime]]
+    [:div.flex.alice-equal-2.equal-sign
+     [:div "="]]
+    [:div.flex.pa.alice-shared-secret
+     [known-number "SHARED SECRET" (mod (js/Math.pow (mod (js/Math.pow @base @bob-secret) @prime) @alice-secret) @prime)]]
+    [:div.flex.pa.bob-name
+     [:h1 "BOB"]]
+    [:div.flex.pa.bob-secret-1
+     [input-number "SECRET #" bob-secret]]
+    [:div.flex.pa.bob-base
+     [known-number "BASE #" @base]]
+    [:div.flex.bob-secret-2
+     [:div.exp
+      [known-number "" @bob-secret]]
+     [:div.mod "%"]]
+    [:div.flex.pa.bob-prime-1
+     [known-number "PRIME #" @prime]]
+    [:div.flex.bob-equal-1.equal-sign
+     [:div "="]]
+    [:div.flex.pa.bob-number
+     [known-number "BOB'S #" (mod (js/Math.pow @base @bob-secret) @prime)]]
+    [:div.flex.pa.bob-alice-number
+     [known-number "ALICE'S #" (mod (js/Math.pow @base @alice-secret) @prime)]]
+    [:div.flex.bob-secret-3
+     [:div.exp
+      [known-number "" @bob-secret]]
+     [:div.mod "%"]]
+    [:div.flex.pa.bob-prime-2
+     [known-number "PRIME #" @prime]]
+    [:div.flex.bob-equal-2.equal-sign
+     [:div "="]]
+    [:div.flex.pa.bob-shared-secret
+     [known-number "SHARED SECRET" (mod (js/Math.pow (mod (js/Math.pow @base @alice-secret) @prime) @bob-secret) @prime)]]]
    [:div
     [:p "Each computer takes the publicly shared "
      [:span.public "BASE number"]
