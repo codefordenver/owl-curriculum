90bcdb5fc73407e571480ae9774d1660a4dc4d53
eemshi <oreneemshi@gmail.com>
2018-04-04 20:29:55 -0600

    updates copy wip

--- src/cljs/owlet/views/experimental/secret_key_exchange.cljs
+++ src/cljs/owlet/views/experimental/secret_key_exchange.cljs
@@ -32,20 +32,21 @@
       "man-in-the-middle (MITM) attack"]
      "."]
     [:p "So when dealing with sensitive information, computers are instructed to scramble (encrypt) the data before sending it out. Once it's received by an authorized server, that server unscrambles (decrypts) the data and uses it however its supposed to."]
-    [:p "To scramble the data, your computer combines it with a random string of bits, called a \"key.\" To unscramble it, the other computer needs to know that key."]
-    [:p "But how do you share that key without exposing it??"]
-    [:p "Instead of sending the key, your computer sends over two numbers: a "
-     [:span.prime "prime number"]
+    [:p "To scramble the data, Computer A combines it with a random string of bits, called an \"encryption key.\" Computer B needs that encryption key in order to unscramble the data."]
+    [:p "But how do you share the key without exposing it??"]
+    [:p "Instead of sending the key, Computer A sends over two numbers: a "
+     [:span.public "PRIME number"]
      ", and a "
-     [:span.base "base number"]
-     ". It doesn't matter if these are intercepted, because they're useless on their own."]
+     [:span.public "BASE number"]
+     ". It doesn't matter if these are intercepted, because they're useless on their own."]]
+   [prime-base-wrapper]
+   [:div
     [:p "Each computer takes the publicly shared "
-     [:span.base "base number"]
-     ", applies its own secret exponent (which is never shared), and then mods the result by that "
-     [:span.prime "prime number"]
-     ", producing a brand new number. Then, those numbers are publicly exchanged."]
-    [:p "This is where the magic happens. Computer A takes Computer B's number, applies its own secret exponent from before, and then mods it by that "
-     [:span.prime "prime number"]
+     [:span.public "BASE number"]
+     ", applies its own secret exponent (which is never shared), and then mods the result by the "
+     [:span.public "PRIME number"]
+     " to produce a brand new number. Then, those numbers are publicly exchanged."]
+    [:p "This is where the magic happens. Computer A takes Computer B's number, applies its own secret exponent from before, and then mods it by the "
+     [:span.public "PRIME number"]
      ". Computer B does the same, using Computer A's number and its own secret exponent."]
-    [:p "The final number will be the same for both; this is their encryption key!"]]
-   [prime-base-wrapper]])
+    [:p "The final number will be the same for both; this is their encryption key!"]]])
