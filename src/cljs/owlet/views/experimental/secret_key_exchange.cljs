b7965903461d9ca7a191f4ce569ce19b3914e283
eemshi <oreneemshi@gmail.com>
2018-04-08 16:51:51 -0600

    update copy, add popover explanations & suggested values

--- src/cljs/owlet/views/experimental/secret_key_exchange.cljs
+++ src/cljs/owlet/views/experimental/secret_key_exchange.cljs
@@ -11,6 +11,8 @@
 
 (def bob-secret (reagent/atom nil))
 
+(def prime-showing? (reagent/atom false))
+
 (defn exp-mod [base exponent modulus]
   (-> (js/bigInt base)
       (.pow exponent)
@@ -29,7 +31,20 @@
                    (if (= "" (-> e .-target .-value))
                      (reset! value nil)
                      (reset! value (js/parseInt (-> e .-target .-value)))))}]
-    [:span.input-group-text title]]])
+    (if (= value prime)
+      [re-com/popover-anchor-wrapper
+           :showing? prime-showing?
+           :position :below-center
+           :anchor [:span.input-group-text.pulse
+                    {:on-mouse-over (handler-fn (reset! prime-showing? true))
+                     :on-mouse-out  (handler-fn (reset! prime-showing? false))}
+                    title]
+           :popover [re-com/popover-content-wrapper
+                     :close-button? false
+                     :body [:div {:style {:text-align "center"
+                                          :font-size "1.2em"}}
+                            "a \"prime\" number is only divisible by itself and 1"]]]
+      [:span.input-group-text title])]])
 
 (defn known-number [title & [value]]
   [:div.input-group.mb-3
@@ -45,20 +60,50 @@
         mod-showing-4? (reagent/atom false)]
     [:div.secret-key-exchange-experimental-view
      [:div.col-xs-12.col-lg-6
-      [:p "When you submit a password or credit card number online, that information gets sent from your computer to another computer (server) over the internet. However, any information traveling over the internet can be intercepted by someone else, in what is called a "
+      [:p "When you submit a password or credit card number online, your computer has to send that data to another computer (server) over the internet. Any data sent over the internet can be easily intercepted, so when dealing with sensitive data, computers are instructed to scramble (encrypt) the data before sending it out, to prevent a "
        [:a {:href "https://us.norton.com/internetsecurity-wifi-what-is-a-man-in-the-middle-attack.html"
             :target "_blank"}
         "man-in-the-middle (MITM) attack"]
        "."]
-      [:p "So when dealing with sensitive information, computers are instructed to scramble (encrypt) the data before sending it out. Once it's received by an authorized server, that server unscrambles (decrypts) the data and uses it however its supposed to."]
-      [:p "To scramble the data, Computer A combines it with a random string of bits, called an \"encryption key.\" Computer B needs that encryption key in order to unscramble the data."]]
+      [:p "To scramble the data, "
+       [:span "Computer A"]
+       " combines it with a random string of bits, called an \"encryption key.\" Once the data is received, "
+       [:span "Computer B"]
+       " unscrambles (decrypts) the data using the same encryption key."]
+      [:p [:span "Then how do they share that key??"]]
+      [:p "Remember, "
+       [:span "Computer A"]
+       " can't just send the key over, or anyone would be able to use it to decrypt your credit card number."]]
      [:div.col-xs-12.col-lg-6
-      [:p [:i "But how do you share the key without exposing it??"]]
-      [:p "Instead of sending the key, Computer A sends over two numbers: a "
-       [:span.prime "PRIME number"]
-       ", and a "
-       [:span.base "BASE number"]
-       ". It doesn't matter if these are intercepted, because they're useless on their own."]]
+      [:p "Instead, "
+       [:span "Computer A"]
+       " sends "
+       [:span "Computer B"]
+       " two numbers: a "
+       [:span.prime "PRIME"]
+       " number, and a "
+       [:span.base "BASE"]
+       " number. It doesn't matter if these are intercepted, because they're useless on their own."]
+      [:p "Then each computer chooses its own "
+       [:span.secret "SECRET"]
+       " number (which is never shared) and uses it to perform a series of calculations with the "
+       [:span.prime "PRIME"]
+       " and "
+       [:span.base "BASE"]
+       " numbers to generate new "
+       [:span.mixed-1 "PUBLIC"]
+       " numbers, which they exchange out in the open."]
+      [:p "After a few more calculations, they will end up with the "
+       [:span.mixed-2 "SAME number (the encryption key)"]
+       " without ever actually sharing it!"]
+      [:p "Today, there are various key exchange methods that rely on various kinds of math. "
+       [:span "This interactive demo is based on the Diffie-Hellman key exchange (DH), one of the earliest practical examples first introduced in 1976.*"]]]
+     [:div.col-xs-12.directions
+      [:h2.mixed-2 "Fill in the white boxes to calculate a shared secret key"]
+      [:h4 "Use the suggested numbers or try your own."
+       [:br] "Put your mouse over the "
+       [:span.pulse-shrink "\"pulsing\""]
+       " areas to learn more!"]]
      [:div#diffie-hellman-grid
       [:div.divider-1]
       [:div.divider-2]
@@ -66,9 +111,9 @@
       [:div.lines
        [:img {:src "img/experimental/lines.png"}]]
       [:div.flex.pa.prime
-       [input-number "PRIME" prime 29]]
+       [input-number "PRIME #" prime 29]]
       [:div.flex.pa.base
-       [input-number "BASE" base 8]]
+       [input-number "BASE #" base 8]]
       [:div.flex.pa.alice-name
        [:h1 "ALICE"]]
       [:div.flex.pa.alice-secret-1
@@ -76,10 +121,9 @@
       [:div.flex.pa.alice-secret-msg
        [:div [:span "<"]
         " Enter a secret number for Alice. For subtle reasons, it should not have any common factors with the "
-        [:span "BASE"]
-        " number"]]
+        [:span.base "BASE #"]]]
       [:div.flex.pa.alice-base
-       [known-number "BASE" @base]]
+       [known-number "BASE #" @base]]
       [:div.flex.alice-secret-2
        [:div.exp
         [known-number "" @alice-secret]]
@@ -97,7 +141,7 @@
                             [:h2 "\"modulo\" operator"]
                             "returns the remainder after dividing two numbers"]]]]]
       [:div.flex.pa.alice-prime-1
-       [known-number "PRIME" @prime]]
+       [known-number "PRIME #" @prime]]
       [:div.flex.alice-equal-1.equal-sign
        [:div "="]]
       [:div.flex.pa.alice-number.mixed-1
@@ -123,11 +167,11 @@
                             [:h2 "\"modulo\" operator"]
                             "returns the remainder after dividing two numbers"]]]]]
       [:div.flex.pa.alice-prime-2
-       [known-number "PRIME" @prime]]
+       [known-number "PRIME #" @prime]]
       [:div.flex.alice-equal-2.equal-sign
        [:div "="]]
       [:div.flex.pa.alice-shared-secret.mixed-2
-       [known-number "SHARED SECRET" (when (and @prime @base @bob-secret @alice-secret)
+       [known-number "SECRET KEY" (when (and @prime @base @bob-secret @alice-secret)
                                        (exp-mod (exp-mod @base @bob-secret @prime)
                                                 @alice-secret
                                                 @prime))]]
@@ -138,10 +182,9 @@
       [:div.flex.pa.bob-secret-msg
        [:div [:span "<"]
         " Enter a secret number for Bob. For subtle reasons, it should not have any common factors with the "
-        [:span "BASE"]
-        " number"]]
+        [:span.base "BASE #"]]]
       [:div.flex.pa.bob-base
-       [known-number "BASE" @base]]
+       [known-number "BASE #" @base]]
       [:div.flex.bob-secret-2
        [:div.exp
         [known-number "" @bob-secret]]
@@ -159,7 +202,7 @@
                             [:h2 "\"modulo\" operator"]
                             "returns the remainder after dividing two numbers"]]]]]
       [:div.flex.pa.bob-prime-1
-       [known-number "PRIME" @prime]]
+       [known-number "PRIME #" @prime]]
       [:div.flex.bob-equal-1.equal-sign
        [:div "="]]
       [:div.flex.pa.bob-number.mixed-1
@@ -185,21 +228,17 @@
                             [:h2 "\"modulo\" operator"]
                             "returns the remainder after dividing two numbers"]]]]]
       [:div.flex.pa.bob-prime-2
-       [known-number "PRIME" @prime]]
+       [known-number "PRIME #" @prime]]
       [:div.flex.bob-equal-2.equal-sign
        [:div "="]]
       [:div.flex.pa.bob-shared-secret.mixed-2
-       [known-number "SHARED SECRET" (when (and @prime @base @alice-secret @bob-secret)
+       [known-number "SECRET KEY" (when (and @prime @base @alice-secret @bob-secret)
                                        (exp-mod (exp-mod @base @alice-secret @prime)
                                                 @bob-secret
                                                 @prime))]]]
      [:div
-      [:p "Each computer takes the publicly shared "
-       [:span.base "BASE number"]
-       ", applies its own secret exponent (which is never shared), and then mods the result by the "
-       [:span.prime "PRIME number"]
-       " to produce a brand new number. Then, those numbers are publicly exchanged."]
-      [:p "This is where the magic happens. Computer A takes Computer B's number, applies its own secret exponent from before, and then mods it by the "
-       [:span.prime "PRIME number"]
-       ". Computer B does the same, using Computer A's number and its own secret exponent."]
-      [:p "The final number will be the same for both; this is their encryption key!"]]]))
+      [:p "*In 2015, computer scientists discovered the "
+       [:a {:href "https://weakdh.org/"
+            :target "_blank"}
+           "Logjam attack"]
+       ", a security vulnerability against Diffie–Hellman key exchanges that use 512-bit to 1024-bit keys. This can be avoided by using prime numbers that are 2048-bit or larger."]]]))
