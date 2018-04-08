bf7d07ebbac934e87be102a481e1dfa7e5bcf386
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-04-08 01:53:08 -0600

    WIP: Filter bar. Basic functionality. TODO: Fix re-rendering of filter bar when filtering from branches view, make sure that when all filters are deselected, the activities displayed are those of the view you initially filtered from, styling

--- src/cljs/owlet/components/top_bar.cljs
+++ src/cljs/owlet/components/top_bar.cljs
@@ -3,10 +3,14 @@
             [owlet.components.search-bar :refer [search-bar]]
             [re-frame.core :as rf]
             [reagent.core :as reagent]
-            [owlet.helpers :refer [class-names]]))
+            [owlet.helpers :refer [class-names]]
+            [camel-snake-kebab.core :refer [->kebab-case]]
+            [cljsjs.jquery]))
 
 (defonce top-bar-classes
   (reagent/atom #{"top-bar"}))
+
+(defonce filters (reagent/atom '()))
 
 (def scroll-delta (atom `(0 0)))
 
@@ -42,22 +46,57 @@
          (.addEventListener contentNodeRef "scroll" #(check-scroll contentNodeRef))))
      :reagent-render
      (fn []
-       [:div.flex-row.box-shadow {:class (class-names @top-bar-classes)}
-        [:a {:href "#"}
-         [:div.owlet-logo-div.flex-row
-          [:img.owlet-owl {:src "../img/owlet-owl.png"
-                           :alt "Owlet home"}]
-          [:div.owlet-text
-            [:h1 "OWLET"]
-            [:p "TECHNOLOGY EDUCATION MADE SIMPLER"]]]]
-        [:div.search-container
-         [search-bar]]
-        [:div.navigation
-         [:a {:href "#/about"}
-          [:button "ABOUT US"]]
-         [:a {:href "#/branches"}
-          [:button "EXPLORE"]]
-         (when @(rf/subscribe [:my-id])
-          [:a {:href "#/settings"}
-           [:button [:i.fa.fa-gear]]])
-         [login-component]]])}))
+       [:div
+        [:div.flex-row.box-shadow {:class (class-names @top-bar-classes)}
+         [:a {:href "#"}
+          [:div.owlet-logo-div.flex-row
+           [:img.owlet-owl {:src "../img/owlet-owl.png"
+                            :alt "Owlet home"}]
+           [:div.owlet-text
+             [:h1 "OWLET"]
+             [:p "TECHNOLOGY EDUCATION MADE SIMPLER"]]]]
+         [:div.search-container
+          [search-bar]]
+         [:div.navigation
+          [:a {:href "#/about"}
+           [:button "ABOUT US"]]
+          [:a {:href "#/branches"}
+           [:button "EXPLORE"]]
+          (when @(rf/subscribe [:my-id])
+           [:a {:href "#/settings"}
+            [:button [:i.fa.fa-gear]]])
+          [login-component]]]
+        (when-not (some #(= @(rf/subscribe [:active-view]) %) [:welcome-view :about-view :settings-view :activity-view])
+          (let [terms '[]]
+            [:div.flex-row.filter-bar
+             [:span.arrow-left]
+             (doall
+               (for [term (shuffle @(rf/subscribe [:filter-bar-terms]))
+                     :let [name (:name term)
+                           type (:type term)]]
+                  (case type
+                    "Branch"   ^{:key (gensym "branch-")}
+                                [:div
+                                 [:input {:type "checkbox"
+                                          :on-click (fn [e]
+                                                      (if (.-checked (.-target e))
+                                                       (rf/dispatch [:filter-activities-by-selected-terms (swap! filters conj term)])
+                                                       (rf/dispatch [:filter-activities-by-selected-terms (reset! filters (remove #(= % term) @filters))])))}]
+                                 [:label name]]
+                    "Platform" ^{:key (gensym "platform-")}
+                                [:div
+                                 [:input {:type "checkbox"
+                                          :on-click (fn [e]
+                                                      (if (.-checked (.-target e))
+                                                       (rf/dispatch [:filter-activities-by-selected-terms (swap! filters conj term)])
+                                                       (rf/dispatch [:filter-activities-by-selected-terms (reset! filters (remove #(= % term) @filters))])))}]
+                                 [:label name]]
+                    "Tag"      ^{:key (gensym "tag-")}
+                                [:div
+                                 [:input {:type "checkbox"
+                                          :on-click (fn [e]
+                                                      (if (.-checked (.-target e))
+                                                       (rf/dispatch [:filter-activities-by-selected-terms (swap! filters conj term)])
+                                                       (rf/dispatch [:filter-activities-by-selected-terms (reset! filters (remove #(= % term) @filters))])))}]
+                                 [:label name]])))
+             [:span.arrow-right]]))])}))
